8d9095e7ab6fccfc513663e80eb7f505f782454a
MikeMiranda24 <42247709+MikeMiranda24@users.noreply.github.com>
2018-08-09 15:31:04 -0600

    Datos de la Farmacia con framework
    con este vamos a estar trabajando , aqui vamos agregar todos datos que tiene melvin .

---
+++ application/controllers/Inicio.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class Inicio extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('Pagina_Principal');
+	}
+}
---
+++ application/views/Pagina_Principal.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Bienvevido Farmacia M.G.&.M.W</title>
+
+	<style>
+
+	
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1></h1>
+
+	<div id="body">
+		<p></p>
+
+		<p></p>
+		<code></code>
+
+		<p></p>
+		<code></code>
+
+		<p> <a href="user_guide/"></a>.</p>
+	</div>
+
+	<p> <strong></strong> </p>
+</div>
+
+</body>
+</html>
