6cbf63d15751298fcd165b1dd6894d59a684dbc5
MikeMiranda24 <42247709+MikeMiranda24@users.noreply.github.com>
2018-08-10 08:26:29 -0600

    Cambios nuevos 10/08/18
    le Agrege bootras a la pagina principal ,talves les parecen

--- application/views/Pagina_Principal.php
+++ application/views/Pagina_Principal.php
@@ -1,33 +1,166 @@
-<!DOCTYPE html>
+
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Bienvevido Farmacia M.G.&.M.W</title>
+  <head>
+    <title>Pagina Principal</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <!--  Fonts and icons  -->
+      <!--     Fonts and icons     -->
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:100,100,100,100|Roboto+Slab:400,700|Material+Icons" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
 
-	<style>
+    <!-- Material Kit CSS -->
+    <link href="assets/css/material-kit.css?v=2.0.4" rel="stylesheet" />
+  </head>
+  <body><nav class="navbar navbar-color-on-scroll navbar-transparent fixed-top navbar-expand-lg"  color-on-scroll="100">
+  <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href=>
+          </a>
 
-	
-	</style>
-</head>
-<body>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
 
-<div id="container">
-	<h1></h1>
+      <div class="collapse navbar-collapse">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                  <i class="material-icons">apps</i> Template
+              </a>
+            </li>
+          </ul>
+      </div>
+  </div>
+</nav>
 
-	<div id="body">
-		<p></p>
 
-		<p></p>
-		<code></code>
-
-		<p></p>
-		<code></code>
-
-		<p> <a href="user_guide/"></a>.</p>
-	</div>
-
-	<p> <strong></strong> </p>
+<div class="page-header header-filter" data-parallax="true" style="background-image: url('assets/img/bg3.jpg')">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 ml-auto mr-auto">
+        <div class="brand text-center">
+          <h1>FARMACIA MG.&.MW</h1>
+          <h3 class="title text-center">Lo Mejor para su Salud</h3>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
+<div class="main main-raised">
+  <div class="container">
+    <div class="section text-center">
+      <h2 class="title"></h2>
+    </div>
+  </div>
+</div>
+<div>
+  <div class="container">
+    <div class="section text-center">
+      <h2 class="title"></h2>
+    </div>
+  </div>
+</div>
+<div>
+  <div class="container">
+    <div class="section text-center">
+      <h2 class="title"></h2>
+    </div>
+  </div>
+</div>
+<div>
+  <div class="container">
+    <div class="section text-center">
+      <h2 class="title"></h2>
+    </div>
+  </div>
+</div>
+<div>
+  <div class="container">
+    <div class="section text-center">
+      <h2 class="title"></h2>
+    </div>
+  </div>
+</div>
+<div>
+  <div>
+  <div class="container">
+    <div class="section text-center">
+      <h2 class="title"></h2>
+    </div>
+  </div>
+</div>
+<div>
+  <div>
+  <div class="container">
+    <div class="section text-center">
+      <h2 class="title"></h2>
+    </div>
+  </div>
+</div>
+<div>
+  <div>
+  <div class="container">
+    <div class="section text-center">
+      <h2 class="title"></h2>
+    </div>
+  </div>
+</div>
+<div>
+  <div>
+  <div class="container">
+    <div class="section text-center">
+      <h2 class="title"></h2>
+    </div>
+  </div>
+</div>
+<div>
+  <div>
+  <div class="container">
+    <div class="section text-center">
+      <h2 class="title"></h2>
+    </div>
+  </div>
+</div>
+<div>
+  <div>
+  <div class="container">
+    <div class="section text-center">
+      <h2 class="title"></h2>
+    </div>
+  </div>
+</div>
+<div>
+  <div>
+  <div class="container">
+    <div class="section text-center">
+      <h2 class="title"></h2>
+    </div>
+  </div>
+</div>
+<div>
 
-</body>
+
+
+<!--   Core JS Files   -->
+<script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
+<script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+<script src="assets/js/plugins/moment.min.js"></script>
+<!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+<script src="assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+<!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+<script src="assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+<!--  Google Maps Plugin  -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+<!-- Place this tag in your head or just before your close body tag. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
+<script src="assets/js/material-kit.js?v=2.0.4" type="text/javascript"></script></body>
 </html>
